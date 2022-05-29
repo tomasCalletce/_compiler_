24b935a916d252bca406131e92b2467dea76a931
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2022-05-29 17:50:50 -0500

    1 commit

---
+++ Elemento.java
@@ -0,0 +1,40 @@
+
+
+public class Elemento {
+    
+       //atributos
+       private char tipo;
+       private double  valor;
+       private int indice;
+       
+       //Métodos
+       public Elemento(){ }
+       public Elemento(char tip,double val,int ind){
+           tipo=tip;
+           valor=val;
+           indice=ind;
+           
+       }
+       
+       public char darTipo(){
+           return tipo;
+       }
+       public double darValor(){
+           return valor;
+       }
+       public int darIndice(){
+           return indice;
+       }
+       public void asignarTipo(char tip){
+           tipo=tip;
+       }
+       public void asignarValor(int val){
+           valor=val;
+       }
+       public void asignarIndice(int ind){
+           
+           indice=ind;
+       } 
+
+   }
+   
---
+++ Lexico.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+/**
+ *
+ * @author SistemaInvestigacion
+ */
+public class Lexico {
+    private ArrayList<Elemento> cadena;
+    //Métodos
+    public Lexico(){
+        cadena = new ArrayList();
+    }
+    
+    public void adicionarElemento(Elemento ele){
+        cadena.add(ele);
+    }
+    
+    public Elemento darElemento(int ind){
+        return cadena.get(ind);
+    }
+    
+    public int darTamañoCadena(){
+        return cadena.size();
+    }
+    
+    public void mostrarLexico(){
+        Elemento ele; 
+        for (int i=0;i<cadena.size();i++){
+            ele=darElemento(i);
+            System.out.println("Tipo "+(char)ele.darTipo()+" Valor "+(double)ele.darValor()+"  Indice "+(int)ele.darIndice());
+            
+        }
+    }
+    public String cadenaLexico(){
+        String cadena1="";
+        Elemento ele1;
+        for(int i=0;i<cadena.size();i++){
+            ele1 = darElemento(i);
+                    
+        cadena1=cadena1+ele1.darTipo();
+        }
+        return cadena1;
+    }    
+}
